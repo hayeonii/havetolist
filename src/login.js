2ca95cd24f06cd9dbc7dc17a0dcc76c0209a4b19
hayeonii <hayexn51@gmail.com>
2022-04-13 23:00:34 +0900

    todo list

--- src/login.js
+++ src/login.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#name_input");
 const greeting = document.querySelector("#greeting");
+const reset = document.querySelector('#reset');
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -16,6 +17,11 @@ function onLoginSubmit(event) {
   function paintGreetings(username) {
     greeting.innerText = `${username} 님, 오늘도 파이팅!`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    reset.innerHTML = `
+    <button type="submit" class="logout">
+      <i class="fa-solid fa-rotate-right"></i>
+    </button>
+    `
   }
   
   
@@ -27,3 +33,13 @@ function onLoginSubmit(event) {
   } else {
     paintGreetings(savedUsername);
   }
+
+  function removeStorage() {
+    localStorage.clear();
+    // window.location.reload();
+  }
+
+
+  const resetBtn = document.querySelector('.logout');
+
+  resetBtn.addEventListener('click', removeStorage);
